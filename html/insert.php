60eee9103d4ae64343703375fc760d29896a67f6
Ubuntu <irobison@me.com>
2023-12-05 23:08:58 +0000

    updates to insert.php

--- html/insert.php
+++ html/insert.php
@@ -1,6 +1,19 @@
 <?php
-include_once"connect.php";
-echo "test";
+include_once "connect.php";
 $itemid =  $_GET['itemid'];
 $itemname = $_GET['itemname'];
 $itemquantity = $_GET['itemq'];
+
+//$sql = "INSERT INTO inventory (ItemID, ItemName, ItemQuantity) VALUES ('$itemid', '$itemname', '$itemquantity')";
+$sql = "INSERT INTO inventory VALUES ('$itemid', '$itemname', '$itemquantity')";
+
+$result = $conn->query($sql);
+echo $result;
+if($conn->query($sql) === TRUE){
+	echo 'It inserted!!!!!!';
+}else{
+	echo 'It didnt.....';
+}
+
+$conn->close();
+?>
