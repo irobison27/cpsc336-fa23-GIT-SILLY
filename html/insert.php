46af45d4fa9410eabd5006959d3a106cd6f968bf
Ubuntu <irobison@me.com>
2023-12-06 01:43:22 +0000

    Added proper functionality for inserting data into the SQL database. Aditional notes were added to insert.php detailing the code's functionality.

--- html/insert.php
+++ html/insert.php
@@ -1,19 +1,31 @@
 <?php
+//This calls on connect.php to gain access to the SQL database.
 include_once "connect.php";
-$itemid =  $_GET['itemid'];
-$itemname = $_GET['itemname'];
-$itemquantity = $_GET['itemq'];
 
-//$sql = "INSERT INTO inventory (ItemID, ItemName, ItemQuantity) VALUES ('$itemid', '$itemname', '$itemquantity')";
-$sql = "INSERT INTO inventory VALUES ('$itemid', '$itemname', '$itemquantity')";
+// This retrieves all of the data from index.html through $_POST
+$itemid = $_POST['itemid'];
+$itemname = $_POST['itemname'];
+$itemquantity = $_POST['itemq'];
 
-$result = $conn->query($sql);
-echo $result;
-if($conn->query($sql) === TRUE){
-	echo 'It inserted!!!!!!';
-}else{
-	echo 'It didnt.....';
-}
+//This creates an SQL query accessing the inventory table inserting placeholder values for ItemID, ItemName, and ItemQuantity
+$sql = "INSERT INTO inventory (ItemID, ItemName, ItemQuantity) VALUES (?, ?, ?)";
 
+//This prepares the SQL database for the query creating a template for the variables to be inserted into. This also acts as a security measure for the database. 
+$stmt = $conn->prepare($sql);
+
+//This is code that checks on whether the prepared connection queuery worked.
+if ($stmt) {
+//This sets the parameters for the data that is being inserted, in specific the parameters correspond to itemid being a string, itemname being a string, and itemquantity being an integer.
+    $stmt->bind_param("ssi", $itemid, $itemname, $itemquantity);
+
+//This checks to see if the statement successfuly executed or not. If it doesn'tit informs what the error was. 
+    if ($stmt->execute()) {
+        echo 'Data inserted successfully!'; }
+    else {
+        echo 'Error when executing the statement.'; }
+} else{
+	echo 'There was a connection error when preparing the statement.';}
+//This closes both the statement preparation and the database connection.
+$stmt->close();
 $conn->close();
 ?>
