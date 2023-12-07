cb5b9bc1000335f4bd8d47bfb5e099ddb3b58cad
Ubuntu <irobison@me.com>
2023-12-07 17:39:33 +0000

    readded index.html as index.php

---
+++ html/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+	<!-- <link rel="stylesheet" href="insert.css" type="text/css" /> -->
+
+<head>
+	<form action="insert.php" method="POST" class="form">
+
+	<input type="text" placeholder="Item ID" class="ItemID" name="itemid" />
+	<input type="text" placeholder="Item Name" class="ItemName" name ="itemname"/>
+	<input type="number" placeholder="Item Quantity" class="ItemQ" name ="itemq" />
+	<button type="submit" class="button">Add Item</button>
+
+	</form>
+
+	<title>index</title>
+</head>
+<body>
+	<p>
+
+
+<head>
+	<form action="remove.php" method="POST" class="form">
+
+	<input type="text" placeholder="Item ID" class="ItemID" name="itemid_del" />
+	<button type="submit" class="button">Delete Item</button>
+	<title>index</title>
+</head>
+
+	</p>
+
+<body>
+	<p>
+GIT SILLY
+</body>
+
+
+<body>
+	<p>
+Simple Inventory System Project
+</body>
+	</p>
+
+<body>
+	<p>
+Team Members:
+Ian Robison,
+Briana Tolleson,
+Sasha Stevens,
+Garrett Ford.
+	</p>
+
+</body>
+</html>
