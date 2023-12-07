50b0cb22cfb465e90b102ed499e0bafbbf3573e1
Ubuntu <irobison@me.com>
2023-12-07 03:13:10 +0000

    remove.php added and functionality working. need to work on checking whether the ItemID exists. currently just displays that it was 'deleted' no matter what.

--- html/remove.php
+++ html/remove.php
@@ -1,4 +1,23 @@
 <?php
-include_once"connect.php";
-echo "test";
-$itemid =  $_GET['itemid'];
+//This calls on connect.php to gain access to the SQL database.
+include_once "connect.php";
+$itemid = $_POST['itemid_del'];
+$validity = isset($itemid);
+// Checks whether the input ItemID exists. Not proper functionality.
+if ($validity) {
+    $sql = "DELETE FROM inventory WHERE ItemID = ?";
+    $stmt = $conn->prepare($sql);
+    if ($stmt) {
+        $stmt->bind_param("s", $itemid);
+        if ($stmt->execute()) {
+            echo 'The statement was executed.'; }
+       	else {
+            echo 'Error when executing the statement.'; }
+    } else {
+        echo 'There was a connection error when preparing the statement.'; }
+    $stmt->close();
+    $conn->close();
+} else {
+	echo 'ItemID does not exist.';
+}
+?>
